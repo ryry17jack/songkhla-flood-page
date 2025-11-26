dd33b59a0e09f643e1e410ef0939920fb0e47364
ryry17jack <jack.wp2022@gmail.com>
2025-11-26 19:07:20 +0700

    update title, desc & favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,10 @@
 import './globals.css';
 
+export const metadata = {
+  title: 'วิทยาลัยเทคนิคสงขลา ปิดระบบชั่วคราว',
+  description: 'วิทยาลัยเทคนิคสงขลา ปิดระบบชั่วคราว เนื่องจากสถานการณ์น้ำท่วม',
+};
+
 export default function RootLayout({
   children,
 }: Readonly<{
